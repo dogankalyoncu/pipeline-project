da218f0538ee9357f2b7bc82f7ae37c2a5049af6
dogankalyoncu <59858045+dogankalyoncu@users.noreply.github.com>
2023-01-06 13:08:51 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Java 2nd version");
+        System.out.println("Hello from Java 3rd version");
     }
 }
